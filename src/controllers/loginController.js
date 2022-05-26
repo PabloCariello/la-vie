d786c2cd4f2f727109cb9c43a0ed88329b9847cf
Pablo Cariello <pgacariello@gmail.com>
2022-05-26 20:36:42 -0300

    ajuste na resposta do loginController e criação da documentação da API

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -13,10 +13,10 @@ const loginController = {
         })
 
         if (!psicologo) {
-            return res.status(400).json('E-mail não cadastrado')
+            return res.status(400).json("E-mail ou senha inválido, verifique e tente novamente")
         }
         if (!bcrypt.compareSync(senha, psicologo.senha)){
-            return res.status(401).json("senha invalida")
+            return res.status(401).json("E-mail ou senha inválido, verifique e tente novamente")
         }
 
         const token = jwt.sign(
